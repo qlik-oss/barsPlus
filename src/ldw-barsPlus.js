37f30093366c3476a1095880a35c841a7332a597
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2019-01-08 16:10:56 +0100

    selecting within bars implemented

--- src/ldw-barsPlus.js
+++ src/ldw-barsPlus.js
@@ -601,7 +601,32 @@ export default {
       .style("opacity", "0")
       .attr("class", "selectable ldwbar")
       .on("click", function (d) {
-        if (d.qElemNumber >= 0) { // Cannot select a measure
+        if (g.self.$scope.g.defDims == 2){ //if we have two Dims
+          if ( d && d.dim2 ){
+            if (g.selectionMode == "QUICK") {
+              g.self.backendApi.selectValues(1, [d.qElemNumber[1]], true);
+              g.self.backendApi.selectValues(0, [d.qElemNumber[0]], true);
+            }
+            else if (g.selectionMode == "CONFIRM") {
+              var t = d3.select(this).classed("selected");
+              g.self.selectValues(1, [d.qElemNumber[1]], true);
+              g.self.selectValues(0, [d.qElemNumber[0]], true);
+
+              // following to address QS bug where clear button does not clear class names
+              g.self.clearSelectedValues = function () {
+                d3.selectAll("#" + g.id + " .selected").classed("selected", false);
+              };
+              d3.selectAll("#" + g.id + " [ldwdim1='" + d.qElemNumber + "']")
+                .classed("selected", !t);
+              d3.select("#" + g.id + " .ldwtooltip")
+                .style("opacity", "0")
+                .transition()
+                .remove
+              ;
+            }
+          }
+        }
+        if (g.self.$scope.g.defDims == 1){
           if (g.selectionMode == "QUICK") {
             g.self.backendApi.selectValues(0, [d.qElemNumber], true);
           }
